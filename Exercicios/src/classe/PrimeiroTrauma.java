246dfa173c3fca62c3c48febe794ed93c1b2a627
Diego Ferreira <33259326+di-ferreira@users.noreply.github.com>
2021-04-29 16:56:09 -0300

    Valor vs Referencia e Primeiro trauma

---
+++ Exercicios/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,15 @@
+package classe;
+
+public class PrimeiroTrauma {
+    int a = 3;
+    static int b = 5;
+
+    public static void main(String[] args) {
+
+        PrimeiroTrauma p = new PrimeiroTrauma();
+        int c = p.a;
+
+        System.out.println(b);
+        System.out.println(c);
+    }
+}

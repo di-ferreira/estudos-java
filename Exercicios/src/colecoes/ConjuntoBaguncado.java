e6df20828b62ac9fea6cc5b3baa8d2d746df1d18
Diego Ferreira <33259326+di-ferreira@users.noreply.github.com>
2021-05-04 14:20:09 -0300

    Collections

---
+++ Exercicios/src/colecoes/ConjuntoBaguncado.java
@@ -0,0 +1,49 @@
+package colecoes;
+
+import java.util.HashSet;
+
+public class ConjuntoBaguncado {
+    public static void main(String[] args) {
+
+        HashSet conjunto = new HashSet();
+
+        conjunto.add(1.2);
+        conjunto.add(true);
+        conjunto.add("Teste");
+        conjunto.add(1);
+        conjunto.add('x');
+
+        System.out.println("Tamanho do conjunto = " + conjunto.size());
+
+        conjunto.add("Teste");
+        conjunto.add('x');
+
+        System.out.println("Tamanho do conjunto = " + conjunto.size());
+
+        System.out.println(conjunto.remove("Teste"));
+        System.out.println(conjunto.remove("teste"));
+        System.out.println(conjunto.remove("x"));
+
+        System.out.println("Tamanho do conjunto = " + conjunto.size());
+
+        System.out.println(conjunto.contains('x'));
+        System.out.println(conjunto.contains(1));
+        System.out.println(conjunto.contains(true));
+
+        HashSet nums = new HashSet();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+
+        System.out.println(nums);
+        System.out.println(conjunto);
+
+        // conjunto.addAll(nums); //União entre dois conjuntos
+        conjunto.retainAll(nums);
+        System.out.println(conjunto);
+
+        conjunto.clear();
+        System.out.println(conjunto);
+
+    }
+}

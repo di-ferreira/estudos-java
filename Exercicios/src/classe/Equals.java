11127e51f9016dee4f0c063adec3b30f5d647250
Diego <diferreira.program@gmail.com>
2021-05-31 09:01:01 -0300

    Classe abstrata

--- Exercicios/src/classe/Equals.java
+++ Exercicios/src/classe/Equals.java
@@ -1,7 +1,5 @@
 package classe;
 
-import java.util.Date;
-
 public class Equals {
     public static void main(String[] args) {
 
@@ -17,7 +15,7 @@ public class Equals {
         System.out.println(u1.equals(u2));
         System.out.println(u2.equals(u1));
 
-        System.out.println(u2.equals(new Date()));
+        System.out.println(u2.equals(new Usuario()));
 
     }
 }

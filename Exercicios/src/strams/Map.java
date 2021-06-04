47b3c0644a283952af8d08415b0028316f2b71d9
Diego <diferreira.program@gmail.com>
2021-06-04 08:27:38 -0300

    Streams map

---
+++ Exercicios/src/strams/Map.java
@@ -0,0 +1,26 @@
+package strams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
+public class Map {
+    public static void main(String[] args) {
+        Consumer<String> print = System.out::print;
+
+        List<String> marcas = Arrays.asList("BMW ", "Audi ", "honda ");
+        marcas.stream().map(m -> m.toUpperCase()).forEach(print);
+
+        UnaryOperator<String> maiuscula = n -> n.toUpperCase();
+        UnaryOperator<String> primeiraLetra = n -> n.charAt(0) + "";
+        UnaryOperator<String> grito = n -> n + "!!!";
+
+        System.out.println("\n\n Usando Composição... ");
+        marcas.stream().map(maiuscula).map(primeiraLetra).map(grito).forEach(print);
+
+        System.out.println("\n\n Usando Composição de outro arquivo... ");
+        marcas.stream().map(Utilitarios.maiuscula).map(primeiraLetra).map(Utilitarios::grito).forEach(print);
+
+    }
+}

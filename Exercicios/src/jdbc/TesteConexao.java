de3d0d1363ad74f1c62a533aa14e6cd93adeb451
Diego Ferreira <diferreira.program@gmail.com>
2021-06-21 14:18:08 -0300

    Conexão com JDBC

---
+++ Exercicios/src/jdbc/TesteConexao.java
@@ -0,0 +1,18 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+    public static void main(String[] args) throws SQLException {
+        final String url = "jdbc:mysql://localhost";
+        final String usuario = "root";
+        final String senha = "$dsf20PV21";
+
+        Connection conexao = DriverManager.getConnection(url,usuario,senha);
+
+        System.out.println("Conexão efetuada com sucesso!");
+        conexao.close();
+    }
+}
